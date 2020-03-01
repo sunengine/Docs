1dabf269afae0eef255a821a5d514293b523c1ec
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-03-01 16:13:19 +0200

    2020-03-01_alpha0.1.2

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  title:'SunEngineDOCs',
+  descriptions:'SunEngine documentation project',
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: '/' },
+      {text: 'en_US', link: '/src/en/' },
+      {text: 'ru_RU', link: '/src/ru/' }
+    ],
+//  sidebar: 'auto'
+    sidebar: [
+      {
+        title: 'Russian',
+        collapsable: false,
+        children: [
+          '/src/ru/'
+        ]
+      },
+      {
+        title: 'English',
+        collapsable: false,
+        children: [
+          '/src/en/'
+        ]
+      }
+    ]
+  }
+}
--- docs/README.md
+++ docs/README.md
@@ -3,7 +3,7 @@ home: true
 heroText: SunEngine.Docs
 tagline: Documentation of SunEngine
 actionText: Choose Language →
-actionLink: /lang/
+actionLink: /src/
 features:
 - title: Simplicity First
   details: Minimal setup with markdown-centered project structure helps you focus on writing.
