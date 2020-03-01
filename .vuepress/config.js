251763344f2d8e5a6d936f907f73cff56ccbb5b7
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-03-01 16:53:10 +0200

    2020-03-01_alpha0.1.3_firstDocsDestination

---
+++ .vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title:'SunEngineDOCs',
+  descriptions:'SunEngine documentation project',
+  dest: 'docs',
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: '/' },
+      {text: 'en_US', link: '/src/en/' },
+      {text: 'ru_RU', link: '/src/ru/' }
+    ],
+//  sidebar: 'auto'
+    sidebar: [
+      {
+        title: 'Russian',
+        collapsable: false,
+        children: [
+          '/src/ru/'
+        ]
+      },
+      {
+        title: 'English',
+        collapsable: false,
+        children: [
+          '/src/en/'
+        ]
+      }
+    ]
+  }
+}
