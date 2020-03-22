345b50610a5d46110a15fd28b0e6262949b8decf
RUBYsimpleMAN <rubysimpleman@gmail.com>
2020-03-22 02:46:38 +0200

    2020-03-22_02-46-38_beta0.0.7_choose1or2or3

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -9,14 +9,86 @@ module.exports = {
       {text: 'en_US', link: '/src/en/' },
       {text: 'ru_RU', link: '/src/ru/' }
     ],
-//  sidebar: 'auto'
+//     sidebar: 'auto'
     sidebar: [
-      {
-        title: 'Russian',
+      { title: 'Russian',
         collapsable: false,
+        children: [ '/src/ru/' ]
+      },
+      
+      { title: 'Администрирование1',
+        collapsable: false,
+        sidebarDepth: 2,
         children: [
-          '/src/ru/'
+          { title: 'Файлы конфигурации',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/configuration_files_ru.html' ] },
+          { title: 'Структура сайта',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/site_structure_ru.html' ] },
+          { title: 'Организация сервиса systemd',
+            collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/systemDmounting_ru.html' ] },
         ]
+      },
+
+      { title: 'Администрирование2',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [
+          { collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/configuration_files_ru.html' ] },
+          { collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/site_structure_ru.html' ] },
+          { collapsable: false,
+            sidebarDepth: 2,
+            children: [ '/src/ru/administration/systemDmounting_ru.html' ] },
+        ]
+      },
+
+
+      // присваивание каждой статье собственного пункта меню:
+      // ТОЖЕ ВАРИАНТ
+      
+      { title: 'Администрирование3',
+      },
+      { title: 'Файлы конфигурации',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [ '/src/ru/administration/configuration_files_ru.html' ]
+      },
+
+      { title: 'Структура сайта',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [ '/src/ru/administration/site_structure_ru.html' ]
+      },
+
+      { title: 'Организация сервиса systemd',
+        collapsable: false,
+        sidebarDepth: 2,
+        children: [ '/src/ru/administration/systemDmounting_ru.html' ]
+      },
+
+
+      { title: 'Разработка',
+        collapsable: false,
+        children: [ '/src/ru/development/directory_structure_ru.html' ],
+        children: [ '/src/ru/development/run_development_ru.html' ],
+        children: [ '/src/ru/development/skins_ru.html' ]
+      },
+      { title: 'Инсталляция',
+        collapsable: false,
+        children: [ '/src/ru/installation/arguments_for_run_ru.html' ],
+        children: [ '/src/ru/installation/deploy_ru.html' ],
+        children: [ '/src/ru/installation/nginx_configuration_ru.html' ],
+        children: [ '/src/ru/installation/prerequisites_ru.html' ],
+        children: [ '/src/ru/installation/step_by_step_server_installation_ru.html' ]
       },
       {
         title: 'English',
@@ -28,3 +100,35 @@ module.exports = {
     ]
   }
 }
+
+// ### Русский
+// 
+// #### Администрирование
+// 
+// [Configuration_files ru_RU](/src/ru/administration/configuration_files_ru.html)
+// 
+// [Site Structure ru_RU](/src/ru/administration/site_structure_ru.html)
+// 
+// [SystemDmounting ru_RU](/src/ru/administration/systemDmounting_ru.html)
+// 
+// 
+// #### Разработка
+// 
+// [Directory_structure ru_RU](/src/ru/development/directory_structure_ru.html)
+// 
+// [Run Development ru_RU](/src/ru/development/run_development_ru.html)
+// 
+// [Skins ru_RU](/src/ru/development/skins_ru.html)
+// 
+// 
+// #### Инсталляция
+// 
+// [Arguments_for_run ru_RU](/src/ru/installation/arguments_for_run_ru.html)
+// 
+// [Deploy ru_RU](/src/ru/installation/deploy_ru.html)
+// 
+// [Nginx_configuration ru_RU](/src/ru/installation/nginx_configuration_ru.html)
+// 
+// [Prerequisites ru_RU](/src/ru/installation/prerequisites_ru.html)
+// 
+// [Step_by_step_server_installation ru_RU](/src/ru/installation/step_by_step_server_installation_ru.html)
